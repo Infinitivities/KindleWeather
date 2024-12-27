189bd70af940b9cb462a2e39aa2b66e2aae24573
刘昊 <infinitivity@qq.com>
2024-12-27 20:39:47 +0800

    a

--- bin/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pkill -f http.server
-RFONT=/usr/java/lib/fonts/Caecilia_LT_65_Medium.ttf
-BFONT=/usr/java/lib/fonts/Caecilia_LT_75_Bold.ttf
-fbink -c 
-fbink -t regular=$RFONT,bold=$BFONT,size=24,top=100,bottom=500,left=25,right=50,format "Weather Server Stoped!"
